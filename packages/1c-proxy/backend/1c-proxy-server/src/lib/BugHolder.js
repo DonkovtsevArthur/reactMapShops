1a5727f57839a693a937a5604f27b3f43396f509
kuchinsn <kuchinsn@lad24.ru>
2018-02-13 17:59:28 +0300

    Рефакторинг ветки 3-1

--- packages/1c-proxy/backend/1c-proxy-server/src/lib/BugHolder.js
+++ packages/1c-proxy/backend/1c-proxy-server/src/lib/BugHolder.js
@@ -9,37 +9,55 @@ const {
   ACTIVE_EXCHANGE,
 } = require('../constants');
 
-const host = process.env.RABBIT_HOST;
-const username = process.env.RABBIT_USER;
-const password = process.env.RABBIT_PASSWORD;
-const expires = process.env.RABBIT_EXPIRES || DEFAULT_EXPIRES;
+// const host = process.env.RABBIT_HOST;
+// const username = process.env.RABBIT_USER;
+// const password = process.env.RABBIT_PASSWORD;
+// const expires = process.env.RABBIT_EXPIRES || DEFAULT_EXPIRES;
 
 class BugHolder {
-  constructor(message) {
-    this.message = {
-      method: message.method,
-      url: message.path,
-      headers: message.headers,
-      payload: message.payload,
-      credentials: message.auth.credentials,
-    };
+  constructor(options) {
+    const { username, password, host, expires } = options;
+    this.username = username;
+    this.password = password;
+    this.host = host;
+    this.expires = expires;
+    // this.message = {
+    //   method: message.method,
+    //   url: message.path,
+    //   headers: message.headers,
+    //   payload: message.payload,
+    //   credentials: message.auth.credentials,
+    // };
   }
-  async send() {
+  async connect() {
     try {
-      this.connection = await rabbit.connect(`amqp://${username}:${password}@${host}`);
-      const ch = await this.connection.createChannel();
-      await ch.assertExchange(HOLD_EXCHANGE, 'fanout');
-      await ch.assertQueue(HOLD_QUEUE, {
-        messageTtl: expires,
+      this.connection =
+        await rabbit
+          .connect(`amqp://${this.username}:${this.password}@${this.host}`);
+      this.chanel = await this.connection.createChannel();
+    } catch (error) {
+      logger.error(error);
+    }
+  }
+  async send(message) {
+    try {
+      await this.chanel.assertExchange(HOLD_EXCHANGE, 'fanout');
+      await this.chanel.assertQueue(HOLD_QUEUE, {
+        messageTtl: this.expires,
         deadLetterExchange: ACTIVE_EXCHANGE,
       });
-      await ch.bindQueue(HOLD_QUEUE, HOLD_EXCHANGE, ROUTING_KEY);
-      const sendMessage = circular.stringify(this.message);
-      await ch.publish(HOLD_EXCHANGE, ROUTING_KEY, new Buffer(sendMessage));
-      ch.close();
+      await this.chanel.bindQueue(HOLD_QUEUE, HOLD_EXCHANGE, ROUTING_KEY);
+      const sendMessage = circular.stringify(message);
+      await this.chanel.publish(HOLD_EXCHANGE, ROUTING_KEY, new Buffer(sendMessage));
     } catch (error) {
       logger.error('Error Send Message to Rabbit ---->', error);
     }
+  }
+  async prepare() {
+
+  }
+  close() {
+    return this.chanel.close();
   }
 }
 
