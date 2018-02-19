3c0f20cb7b87071638246c35defad505420fa2d3
kuchinsn <kuchinsn@lad24.ru>
2018-02-19 16:44:44 +0300

    monoruk-1c-client. Исправил баг в выборки то 5 должников

--- packages/monoruk/frontend/1c-client/src/js/helper/queryGenerator.ts
+++ packages/monoruk/frontend/1c-client/src/js/helper/queryGenerator.ts
@@ -3,30 +3,45 @@ import { WidgetConfig } from '../reducers/widgets';
 
 function formatDate(period: string) {
   switch (period) {
-  case 'month':
-    return Moment().add(-1, 'month').utc().format('YYYY-MM-DD HH:mm:00');
-  case 'quarter':
-    return Moment().add(-4, 'month').utc().format('YYYY-MM-DD HH:mm:00');
-  case 'half-year':
-    return Moment().add(-6, 'month').utc().format('YYYY-MM-DD HH:mm:00');
-  case 'year':
-    return Moment().add(-12, 'month').utc().format('YYYY-MM-DD HH:mm:00');
-  default:
-    return Moment().add(-1, 'month').utc().format('YYYY-MM-DD HH:mm:00');
+    case 'month':
+      return Moment()
+        .add(-1, 'month')
+        .utc()
+        .format('YYYY-MM-DD HH:mm:00');
+    case 'quarter':
+      return Moment()
+        .add(-4, 'month')
+        .utc()
+        .format('YYYY-MM-DD HH:mm:00');
+    case 'half-year':
+      return Moment()
+        .add(-6, 'month')
+        .utc()
+        .format('YYYY-MM-DD HH:mm:00');
+    case 'year':
+      return Moment()
+        .add(-12, 'month')
+        .utc()
+        .format('YYYY-MM-DD HH:mm:00');
+    default:
+      return Moment()
+        .add(-1, 'month')
+        .utc()
+        .format('YYYY-MM-DD HH:mm:00');
   }
 }
 
 function getDateLimit(dateField: string, widgetConfig: WidgetConfig) {
-  const downDateLimit = widgetConfig.period ?
-  `${dateField} > toDateTime('${formatDate(widgetConfig.period)}')` :
-  '';
+  const downDateLimit = widgetConfig.period
+    ? `${dateField} > toDateTime('${formatDate(widgetConfig.period)}')`
+    : '';
   return downDateLimit ? `WHERE ${downDateLimit}` : '';
 }
 
 export function queryGeneratorDebtors5() {
   return `SELECT
             anyLast(debtors5) as x
-          FROM ( SELECT * FROM dataSource ORDER BY timestamp )`;
+          FROM ( SELECT * FROM dataSource ORDER BY timestamp, dateDoc)`;
 }
 
 export default (widget: WidgetConfig) => {
@@ -35,7 +50,9 @@ export default (widget: WidgetConfig) => {
             anyLast(y) as y,
             toDate(x) as x
           FROM (
-            SELECT dateDoc AS x, ${widget.index} AS y, timestamp FROM dataSource ${dateLimit} ORDER BY timestamp
+            SELECT dateDoc AS x, ${
+              widget.index
+            } AS y, timestamp FROM dataSource ${dateLimit} ORDER BY timestamp
           )
           GROUP BY x`;
 };
