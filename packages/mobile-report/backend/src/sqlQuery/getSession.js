c7a0f6c89898ac065506162dcc2ddd29245686aa
kuchinsn <kuchinsn@lad24.ru>
2018-03-14 14:25:55 +0300

    Изменение запроса выборки сессий в мобильном отчете

--- packages/mobile-report/backend/src/sqlQuery/getSession.js
+++ packages/mobile-report/backend/src/sqlQuery/getSession.js
@@ -11,30 +11,44 @@ module.exports = (uuid, ltCloseDate, gtCloseDate, storeUuid, steps, offset) => {
     min = `plus(toDateTime(toDate(plus(${gtCloseDate}, timeZone))), ${free ? 0 : offset})`;
   }
   return `
-  SELECT DISTINCT
-    toUInt32OrZero(sessionNumber) as sessionNumber,
-    floor(sumIf(closeResultSum, documentType ='SELL'), 2) as sum,
-    floor(sumIf(closeResultSum, or(documentType ='SELL', documentType ='PAYBACK') ), 2) as total,
-    plus(minIf(openDate, documentType ='OPEN_SESSION'), any(timeZone)) as openDate,
-    plus(maxIf(closeDate, documentType ='CLOSE_SESSION'), any(timeZone)) as closeDate,
-    countIf(documentUuid, documentType ='SELL') as receipts,
-    any(userUuid) as user,
-    countIf(documentUuid, documentType = 'PAYBACK') as paybacks,
-    floor(sumIf(closeResultSum, documentType = 'PAYBACK'), 2) as paybacksSum,
-    floor(sumIf(closeResultSum, and(paymentType = 'CASH', documentType = 'PAYBACK')), 2) as sumCashPayback,
-    floor(sumIf(closeResultSum, and(paymentType = 'CARD', documentType = 'PAYBACK')), 2) as sumCardPayback,
-    floor(sumIf(closeResultSum, and(paymentType = 'CASH', documentType = 'SELL')), 2) as sumCashSell,
-    floor(sumIf(closeResultSum, and(paymentType = 'CARD', documentType = 'SELL')), 2) as sumCardSell
-  FROM (
-    SELECT DISTINCT * FROM documents
-    WHERE 
-      plus(closeDate, timeZone) <= ${max} AND 
-      plus(closeDate, timeZone) >= ${min} AND 
-      userUuid = '${uuid}' AND 
-      storeUuid = '${storeUuid}'
-  )
-  GROUP BY
-    sessionNumber
-  ORDER BY sessionNumber DESC
-  `;
+  SELECT
+  toUInt32OrZero(sessionNumber) as sessionNumber,
+  floor(sum(docSum), 2) as sum,
+  floor(sum(docTotal), 2) as total,
+  max(docOpenDate) as openDate,
+  max(docCloseDate) as closeDate,
+  countIf(documentUuid, docDocumentType = 'SELL') as receipts,
+  any(docUser) as user,
+  countIf(documentUuid, docDocumentType = 'PAYBACK') as paybacks,
+  floor(sum(docPaybacksSum), 2) as paybacksSum,
+  floor(sum(docSumCashPayback), 2) as sumCashPayback,
+  floor(sum(docSumCardPayback), 2) as sumCardPayback,
+  floor(sum(docSumCashSell), 2) as sumCashSell,
+  floor(sum(docSumCardSell), 2) as sumCardSell
+FROM (
+  SELECT
+      any(sessionNumber) as sessionNumber,
+      anyIf(closeResultSum, documentType ='SELL') as docSum,
+      anyIf(closeResultSum, or(documentType ='SELL', documentType ='PAYBACK')) as docTotal,
+      plus(anyIf(openDate, documentType ='OPEN_SESSION'), any(timeZone)) as docOpenDate,
+      plus(anyIf(closeDate, documentType ='CLOSE_SESSION'), any(timeZone)) as docCloseDate,
+      documentUuid,
+      any(documentType) as docDocumentType,
+      any(userUuid) as docUser,
+      anyIf(closeResultSum, documentType = 'PAYBACK') as docPaybacksSum,
+      anyIf(closeResultSum, and(paymentType = 'CASH', documentType = 'PAYBACK')) as docSumCashPayback,
+      anyIf(closeResultSum, and(paymentType = 'CARD', documentType = 'PAYBACK')) as docSumCardPayback,
+      anyIf(closeResultSum, and(paymentType = 'CASH', documentType = 'SELL')) as docSumCashSell,
+      anyIf(closeResultSum, and(paymentType = 'CARD', documentType = 'SELL')) as docSumCardSell
+  FROM documents
+  WHERE 
+    plus(closeDate, timeZone) <= ${max} AND 
+    plus(closeDate, timeZone) >= ${min} AND 
+    userUuid = '${uuid}' AND 
+    storeUuid = '${storeUuid}'
+  GROUP BY documentUuid
+)
+GROUP BY
+  sessionNumber
+ORDER BY sessionNumber DESC`;
 };
